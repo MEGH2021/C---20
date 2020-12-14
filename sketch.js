4999bb723bc6f0e4753c45c1b671d176df20a4d0
MEGH2021 <75684990+MEGH2021@users.noreply.github.com>
2020-12-14 21:13:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
+var fixedRect, movingRect;
+
 function setup() {
-  createCanvas(1600,756);
-  fixedRect = createSprite(200, 200, 50, 80);
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
   fixedRect.shapeColor = "green";
-  movingRect = createSprite(400, 200, 80, 30);
-  movingRect.shapeColor = "blue";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background("black");  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-  console.log(movingRect.x - fixedRect.x)
+  background(0,0,0);  
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
-    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 
-    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "yellow";
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
   }
-  else {
-    fixedRect.shapeColor = "green";
-    movingRect.shapeColor = "blue";
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
   }
-
-  
-
   drawSprites();
 }
